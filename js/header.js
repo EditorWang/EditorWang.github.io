7e93245e7fe9850109d8aab6aebaf0a363147519
gaonengcrazy <llkkjjhhllkkjjhh@163.com>
2020-04-05 14:04:09 +0800

    Site updated: 2020-04-05 14:04:07

---
+++ js/header.js
@@ -0,0 +1,14 @@
+$(function () {
+    /*
+    $('.Index_Link').addClass('active');
+    $('li').click(function (e) { 
+        e.preventDefault();
+        window.open(this.firstChild.href,'_self')
+        
+        $(this.firstChild).addClass('active');
+        $(this.firstChild).css('color', 'blue');
+        console.log(this.firstChild)
+        console.log(this.firstChild)
+    });
+    */
+});
